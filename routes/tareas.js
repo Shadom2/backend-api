251ed83c1aa6695fd25889b527d4ebcb91032396
Estudiante Ltic <estudiante@sistemas.local>
2025-06-17 23:28:48 -0500

    arreglo de tareas

--- routes/tareas.js
+++ routes/tareas.js
@@ -7,6 +7,7 @@ router.get('/',tareasController.getTareas)
 
 router.post('/', tareasController.addTarea)
 
-router.delete('/:id', tareasController.deleteTarea);
+router.delete('/:id', tareasController.eliminarTarea);
+
 
 module.exports = router;
